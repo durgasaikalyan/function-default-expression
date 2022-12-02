284f3a276bea524e57f358550a9e04ce50138485
durgasaikalyan <durgasaikalyanccbp@gmail.com>
2022-12-02 17:00:53 +0000

    your message

---
+++ exportFunction.js
@@ -0,0 +1,9 @@
+function name(text) {
+  result = "This is a Function";
+  return result;
+}
+module.exports = name;
+
+// module.exports = function anExportedFunc() {
+//   return "yup simple as that";
+// };
